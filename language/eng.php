dd10d02f3af6da8646e0735ba663bf306ee0493d
20matteo00 <matteo.moscone.1atgiorgi@gmail.com>
2025-03-08 16:29:59 +0100

    as

--- language/eng.php
+++ language/eng.php
@@ -1,33 +1,28 @@
 <?php
 // language/eng.php
-// HOME PAGE
+// Generali
 define("NOMESITO", "Simulator");
+
+// Navbar
 define("COMPETIZIONI", "Competition");
-// REGISTRATION PAGE
+define("CAMPIONATI", "League");
+define("SQUADRE", "Teams");
 define("REGISTRATI", "Registration");
 define("ACCEDI", "Login");
+define("BENVENUTO", "Welcome");
+define("ESCI", "Logout");
+
+// Form (login)
 define("NOME_UTENTE", "Username");
 define("EMAIL", "Email");
 define("PASSWORD", "Password");
 define("CONFERMA_PASSWORD", "Confirm Password");
-define("HAI_GIA_UN_ACCOUNT", "Already have an account?");
-define("BENVENUTO", "Welcome");
-define("ESCI", "Logout");
-define("TUTTI_I_CAMPI_OBBLIGATORI", "All fields are required");
-define("PASSWORD_NON_UGUALI", "Passwords do not match");
-define("EMAIL_GIA_REGISTRATA", "Email already registered");
-define("REGISTRAZIONE_AVVENUTA", "Registration successful");
-define("NON_HAI_UN_ACCOUNT", "Don't have an account?");
-define("UTENTE_NON_TROVATO", "User not found");
-define("LOGIN_AVVENUTO", "Login successful");
-define("PASSWORD_ERRATA", "Wrong password");
-define("PASSWORD_DIMENTICATA", "Forgot your password?");
-define("RECUPERA", "Recover it here");
 define("VERIFICA", "Verify");
 define("NUOVA_PASSWORD", "New Password");
 define("AGGIORNA_PASSWORD", "Update Password");
 define("RECUPERA_PASSWORD", "Recover Password");
-define("PASSWORD_AGGIORNATA_SUCCESSO", "Password updated successfully! Redirecting...");
+
+// Form (profilo)
 define("PROFILO", "Profile");
 define("NOME", "Name");
 define("COGNOME", "Surname");
@@ -35,7 +30,23 @@ define("DATA_NASCITA", "Date of Birth");
 define("LUOGO_NASCITA", "Place of Birth");
 define("TELEFONO", "Phone");
 define("AGGIORNA_PROFILO", "Update Profile");
+
+//Info
+define("HAI_GIA_UN_ACCOUNT", "Already have an account?");
+define("NON_HAI_UN_ACCOUNT", "Don't have an account?");
+define("RECUPERA", "Recover it here");
+
+// Errori
+define("TUTTI_I_CAMPI_OBBLIGATORI", "All fields are required");
+define("PASSWORD_NON_UGUALI", "Passwords do not match");
+define("EMAIL_GIA_REGISTRATA", "Email already registered");
+define("UTENTE_NON_TROVATO", "User not found");
+define("PASSWORD_ERRATA", "Wrong password");
+define("PASSWORD_DIMENTICATA", "Forgot your password?");
+
+// Successo
+define("REGISTRAZIONE_AVVENUTA", "Registration successful");
+define("LOGIN_AVVENUTO", "Login successful");
+define("PASSWORD_AGGIORNATA_SUCCESSO", "Password updated successfully! Redirecting...");
 define("DATI_AGGIORNATI", "Data updated successfully");
-define("CAMPIONATI", "League");
-define("SQUADRE", "Teams");
 ?>
--- language/ita.php
+++ language/ita.php
@@ -1,33 +1,28 @@
 <?php
 // language/ita.php
-// HOME PAGE
+// Generali
 define("NOMESITO", "Simulatore");
+
+// Navbar
 define("COMPETIZIONI", "Competizioni");
-// REGISTRATION PAGE
+define("CAMPIONATI", "Campionati");
+define("SQUADRE", "Squadre");
 define("REGISTRATI", "Registrati");
 define("ACCEDI", "Accedi");
+define("BENVENUTO", "Benvenuto");
+define("ESCI", "Esci");
+
+// Form (login)
 define("NOME_UTENTE", "Nome Utente");
 define("EMAIL", "Email");
 define("PASSWORD", "Password");
 define("CONFERMA_PASSWORD", "Conferma Password");
-define("HAI_GIA_UN_ACCOUNT", "Hai già un account?");
-define("BENVENUTO", "Benvenuto");
-define("ESCI", "Esci");
-define("TUTTI_I_CAMPI_OBBLIGATORI", "Tutti i campi sono obbligatori");
-define("PASSWORD_NON_UGUALI", "Le password non corrispondono");
-define("EMAIL_GIA_REGISTRATA", "Email già registrata");
-define("REGISTRAZIONE_AVVENUTA", "Registrazione avvenuta con successo");
-define("NON_HAI_UN_ACCOUNT", "Non hai un account?");
-define("UTENTE_NON_TROVATO", "Utente non trovato");
-define("LOGIN_AVVENUTO", "Login avvenuto con successo");
-define("PASSWORD_ERRATA", "Password errata");
-define("PASSWORD_DIMENTICATA", "Password dimenticata?");
-define("RECUPERA", "Recuperala qui");
 define("VERIFICA", "Verifica");
 define("NUOVA_PASSWORD", "Nuova Password");
 define("AGGIORNA_PASSWORD", "Aggiorna Password");
 define("RECUPERA_PASSWORD", "Recupera Password");
-define("PASSWORD_AGGIORNATA_SUCCESSO", "Password aggiornata con successo! Reindirizzamento in corso...");
+
+// Form (profilo)
 define("PROFILO", "Profilo");
 define("NOME", "Nome");
 define("COGNOME", "Cognome");
@@ -35,7 +30,23 @@ define("DATA_NASCITA", "Data di Nascita");
 define("LUOGO_NASCITA", "Luogo di Nascita");
 define("TELEFONO", "Telefono");
 define("AGGIORNA_PROFILO", "Aggiorna Profilo");
+
+// Info
+define("HAI_GIA_UN_ACCOUNT", "Hai già un account?");
+define("NON_HAI_UN_ACCOUNT", "Non hai un account?");
+define("RECUPERA", "Recuperala qui");
+
+// Errori
+define("TUTTI_I_CAMPI_OBBLIGATORI", "Tutti i campi sono obbligatori");
+define("PASSWORD_NON_UGUALI", "Le password non corrispondono");
+define("EMAIL_GIA_REGISTRATA", "Email già registrata");
+define("UTENTE_NON_TROVATO", "Utente non trovato");
+define("PASSWORD_ERRATA", "Password errata");
+define("PASSWORD_DIMENTICATA", "Password dimenticata?");
+
+// Successo
+define("REGISTRAZIONE_AVVENUTA", "Registrazione avvenuta con successo");
+define("LOGIN_AVVENUTO", "Login avvenuto con successo");
+define("PASSWORD_AGGIORNATA_SUCCESSO", "Password aggiornata con successo! Reindirizzamento in corso...");
 define("DATI_AGGIORNATI", "Dati aggiornati con successo");
-define("CAMPIONATI", "Campionati");
-define("SQUADRE", "Squadre");
 ?>
